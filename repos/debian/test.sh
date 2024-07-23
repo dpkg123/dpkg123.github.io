aca0bead66254401dca308b306e24cdfcfdb3589
dpkg123 <test1@qw5d.onmicrosoft.com>
2024-07-23 14:38:56 +0800

    Site updated: 2024-07-23 14:38:30

--- repos/debian/test.sh
+++
@@ -1,4 +0,0 @@
-TOKEN=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                                                
-OWNER=chrismaltby                                                                             
-REPO=gb-studio                                                                                
-bash wget.sh
